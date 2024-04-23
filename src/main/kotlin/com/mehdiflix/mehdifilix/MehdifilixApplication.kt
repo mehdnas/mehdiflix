6e2b58a8336a80c5e4b1f7f27796b0510f9b60d9
Mehdi Nasef <mehdinasefpu@gmail.com>
2024-04-23 16:08:28 +0200

    Add initial spring initializr files

---
+++ src/main/kotlin/com/mehdiflix/mehdifilix/MehdifilixApplication.kt
@@ -0,0 +1,11 @@
+package com.mehdiflix.mehdifilix
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MehdifilixApplication
+
+fun main(args: Array<String>) {
+	runApplication<MehdifilixApplication>(*args)
+}
---
+++ src/test/kotlin/com/mehdiflix/mehdifilix/MehdifilixApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mehdiflix.mehdifilix
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MehdifilixApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
